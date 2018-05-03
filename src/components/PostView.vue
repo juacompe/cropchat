9580160308ea5a5d4f20fc98b7ab6ce3ebcef8fb
juacompe <juacompe@gmail.com>
2018-05-03 10:48:36 +0700

    load a random cat from the CatAPI

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -22,12 +22,26 @@
 </template>
 
 <script>
+import parse from 'xml-parser'
 export default {
+  data () {
+    return {
+      catUrl: null
+    }
+  },
+  mounted () {
+    this.$http
+      .get('https://thecatapi.com/api/images/get?format=xml&results_per_page=1')
+      .then(response => {
+        this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
+      })
+  },
+  methods: {}
 }
 </script>
 <style scoped>
-  .waiting {
-    padding: 10px;
-    color: #555;
-  }
+.waiting {
+  padding: 10px;
+  color: #555;
+}
 </style>
