49ac32f85ce51f463abd348e54a436ee110165a9
juacompe <juacompe@gmail.com>
2018-05-03 10:30:59 +0700

    We are now connected to our database.

---
+++ src/service/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyBQ1mbKl7g6aylbxezescV1Wgq9Dar8aGo",
+  authDomain: "cropchat-48e55.firebaseapp.com",
+  databaseURL: "https://cropchat-48e55.firebaseio.com",
+  projectId: "cropchat-48e55",
+  storageBucket: "cropchat-48e55.appspot.com",
+  messagingSenderId: "3089234757"
+}
+
+firebase.initializeApp(config)
+
+export default {
+  database: firebase.database()
+}
